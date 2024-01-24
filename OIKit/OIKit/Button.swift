d9ecd372d9a42c24b55fd0022709681132a3f5e0
Keen <zhekeen90@gmail.com>
2024-01-24 15:55:16 +0700

    Button

--- OIKit/OIKit/Button.swift
+++ OIKit/OIKit/Button.swift
@@ -7,20 +7,21 @@
 
 import UIKit
 
+private var actionKeys: UInt8 = 0
 extension UIButton {
 
-    typealias Action = () -> Void
-
+    public typealias Action = () -> Void
+    
     private struct AssociatedKeys {
         static var actionKey = "actionKey"
     }
 
     private var action: Action? {
         get {
-            return objc_getAssociatedObject(self, &AssociatedKeys.actionKey) as? Action
+            return objc_getAssociatedObject(self, &actionKeys) as? Action
         }
         set {
-            objc_setAssociatedObject(self, &AssociatedKeys.actionKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, &actionKeys, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
 
@@ -30,7 +31,7 @@ extension UIButton {
     }
 
     @discardableResult
-    func content(_ action: @escaping Action, label block: (UIButton) -> Void) -> UIButton {
+    public func content(_ action: @escaping Action, label block: (UIButton) -> Void) -> UIButton {
         block(self)
         self.action = action
         addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
@@ -41,20 +42,20 @@ extension UIButton {
         action?()
     }
 
-    func frame(width: CGFloat, height: CGFloat) {
+    public func frame(width: CGFloat, height: CGFloat) {
         translatesAutoresizingMaskIntoConstraints = false
         widthAnchor.constraint(equalToConstant: width).isActive = true
         heightAnchor.constraint(equalToConstant: height).isActive = true
     }
     
     @discardableResult
-    func textColor(_ color: UIColor, for state: UIControl.State = .normal) -> UIButton {
+    public func textColor(_ color: UIColor, for state: UIControl.State = .normal) -> UIButton {
         setTitleColor(color, for: state)
         return self
     }
     
     @discardableResult
-    func textColor(_ hex: UInt32, for state: UIControl.State = .normal) -> UIButton {
+    public func textColor(_ hex: UInt32, for state: UIControl.State = .normal) -> UIButton {
         let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
         let green = CGFloat((hex & 0x00FF00) >> 8) / 255.0
         let blue = CGFloat(hex & 0x0000FF) / 255.0

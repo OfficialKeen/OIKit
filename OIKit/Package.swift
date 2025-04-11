f21ef6f9bc11d95e2653c1bea16554f210b8a57f
Keen <zhekeen90@gmail.com>
2025-04-11 21:20:58 +0700

    Add Package

---
+++ OIKit/Package.swift
@@ -0,0 +1,28 @@
+//
+//  Package.swift
+//  OIKit
+//
+//  Created by keenoi on 11/04/25.
+//
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "OIKit", // Nama framework lo
+    platforms: [
+        .iOS(.v13), // Minimal iOS version yang didukung
+    ],
+    products: [
+        .library(
+            name: "OIKit", // Nama library yang dihasilkan
+            targets: ["OIKit"] // Target yang dibuild
+        ),
+    ],
+    targets: [
+        .target(
+            name: "OIKit", // Nama target yang akan dibuild
+            path: "OIKit" // Path menuju folder OIKit yang berisi semua file framework
+        ),
+    ]
+)
